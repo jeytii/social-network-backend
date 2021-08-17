c1ffb4d2113ef27b51363643de0a50fdec89fdba
Jonathan Tarnate <jstarnate16@gmail.com>
2021-08-18 01:31:36 +0800

    Created a controller for authentication logics.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        abort_if(!Auth::attempt($credentials), 422, 'Cannot find username and password combination.');
+
+        $token = $request->user()->createToken(env('SANCTUM_SECRET_KEY'))->plainTextToken;
+
+        return response()->json(compact('token'));
+    }
+}
