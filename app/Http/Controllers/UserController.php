8cc9568aca64bd0631a392ab663264cdcaaa51f6
Jonathan Tarnate <jstarnate16@gmail.com>
2021-08-23 21:40:47 +0800

    Added a method for getting paginated users.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function get()
+    {
+        $paginated = User::paginate(20);
+        $data = collect($paginated)->only(['data', 'next_page_url']);
+
+        return response()->json($data);
+    }
 }
