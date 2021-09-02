b948276520c592b8ebd9be26df90ccd37caf5187
Jonathan Tarnate <jstarnate16@gmail.com>
2021-09-02 18:37:37 +0800

    Added a method for getting the list of posts.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    // 
+    /**
+     * Get paginated news feed posts.
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function get()
+    {
+        $ids = auth()->user()->following()->pluck('id')->merge(auth()->id());
+        $data = Post::whereHas('user', fn($q) => $q->whereIn('id', $ids))
+                    ->with('user:id,slug,name,username,gender,image_url')
+                    ->withCount([
+                        'likers as likes_count',
+                        'comments'
+                    ])
+                    ->orderByDesc('created_at')
+                    ->paginate(20);
+
+        $hasMore = $data->hasMorePages();
+        $nextOffset = $hasMore ? $data->currentPage() + 1 : null;
+
+        return response()->json([
+            'data' => $data->items(),
+            'has_more' => $hasMore,
+            'next_offset' => $nextOffset,
+        ]);
+    }
 }
