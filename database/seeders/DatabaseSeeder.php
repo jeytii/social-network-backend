97c0f6f564b3e48cafffa6429a3acd5269a669c0
Jonathan Tarnate <jstarnate16@gmail.com>
2021-09-04 23:14:59 +0800

    Chained magic method for creation of posts and comments.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(100)->create();
+        User::factory(5)->hasPosts(5)->hasComments(5)->create();
     }
 }
