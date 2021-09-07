d2dd96d17fa88e10583933de8c27e620cfd58b91
Jonathan Tarnate <jstarnate16@gmail.com>
2021-09-07 20:12:30 +0800

    Added a test for "show more" operation.

--- tests/Feature/API/Comment/GetListsTest.php
+++ tests/Feature/API/Comment/GetListsTest.php
@@ -2,9 +2,12 @@
 
 use App\Models\{User, Post};
 use Illuminate\Support\Facades\DB;
+use Illuminate\Foundation\Testing\WithFaker;
+
+uses(WithFaker::class);
 
 beforeAll(function() {
-    User::factory(3)->hasPosts(3)->hasComments(3)->create();
+    User::factory(3)->hasPosts(3)->create();
 });
 
 afterAll(function() {
@@ -41,12 +44,22 @@ test('Should return an empty list if a selected post doesn\'t have any comment',
 });
 
 test('Should return a paginated list of comments under a specific post', function() {
-    $slug = Post::has('comments')->first()->slug;
+    $comments = collect(range(1,8))->map(fn() => [
+        'post_id' => 1,
+        'body' => $this->faker->words(3, true),
+    ])->toArray();
+    
+    $this->user->comments()->createMany($comments);
+
+    $slug = Post::first()->slug;
     
     // Suppose the selected post only has less than 20 comments
     $this->response
         ->getJson("/api/comments?pid={$slug}&page=1")
         ->assertOk()
+        ->assertJsonCount(8, 'data')
+        ->assertJsonPath('has_more', false)
+        ->assertJsonPath('next_offset', null)
         ->assertJsonStructure([
             'data' => [
                 '*' => [
@@ -66,9 +79,7 @@ test('Should return a paginated list of comments under a specific post', functio
             ],
             'has_more',
             'next_offset'
-        ])
-        ->assertJsonPath('has_more', false)
-        ->assertJsonPath('next_offset', null);
+        ]);
 
     $this->response
         ->getJson("/api/comments?pid={$slug}&page=2")
@@ -78,4 +89,29 @@ test('Should return a paginated list of comments under a specific post', functio
             'has_more' => false,
             'next_offset' => null,
         ]);
+});
+
+test('Should return more comments upon clicking "show more"', function() {
+    $slug = Post::first()->slug;
+
+    $this->response
+        ->getJson("/api/comments/more?pid={$slug}&page=1")
+        ->assertOk()
+        ->assertJsonCount(5, 'data')
+        ->assertJsonPath('has_more', true)
+        ->assertJsonPath('next_offset', 2);
+
+    $this->response
+        ->getJson("/api/comments/more?pid={$slug}&page=2")
+        ->assertOk()
+        ->assertJsonCount(3, 'data')
+        ->assertJsonPath('has_more', false)
+        ->assertJsonPath('next_offset', null);
+
+    $this->response
+        ->getJson("/api/comments/more?pid={$slug}&page=3")
+        ->assertOk()
+        ->assertJsonCount(0, 'data')
+        ->assertJsonPath('has_more', false)
+        ->assertJsonPath('next_offset', null);
 });
