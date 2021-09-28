d91be9feb1699a513b0d1014be456cee6fc1b20a
Jonathan Tarnate <jstarnate16@gmail.com>
2021-09-29 04:48:32 +0800

    Switched to Repository-Service pattern.

---
+++ app/Services/PostService.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\{User, Post};
+use App\Http\Requests\CreateOrUpdateLongTextRequest;
+use App\Notifications\NotifyUponAction;
+
+class PostService
+{
+    /**
+     * Create a post.
+     * 
+     * @param \App\Http\Requests\CreateOrUpdateLongTextRequest  $request
+     * @return array
+     */
+    public function createPost(CreateOrUpdateLongTextRequest $request): array
+    {
+        $data = $request->user()->posts()
+                    ->create($request->only('body'))
+                    ->withUser()
+                    ->withCount(['likers as likes_count', 'comments'])
+                    ->first();
+
+        return [
+            'status' => 201,
+            'message' => 'Successfully created a post.',
+            'data' => $data,
+        ];
+    }
+
+    /**
+     * Update a post.
+     * 
+     * @param \App\Http\Requests\CreateOrUpdateLongTextRequest  $request
+     * @param \App\Models\Post  $post
+     * @return array
+     */
+    public function updatePost(CreateOrUpdateLongTextRequest $request, Post $post): array
+    {
+        $request->user()->posts()
+            ->find($post->id)
+            ->update($request->only('body'));
+
+        return [
+            'status' => 200,
+            'message' => 'Successfully updated a post.',
+        ];
+    }
+
+    /**
+     * Delete a post.
+     * 
+     * @param \App\Models\User  $user
+     * @param string  $postId
+     * @return array
+     */
+    public function deletePost(User $user, string $postId): array
+    {
+        $user->posts()->find($postId)->delete();
+
+        return [
+            'status' => 200,
+            'message' => 'Successfully deleted a post.',
+        ];
+    }
+
+    /**
+     * Like a post.
+     * 
+     * @param \App\Models\User  $liker
+     * @param \App\Models\Post  $post
+     * @return array
+     */
+    public function likePost(User $liker, Post $post): array
+    {
+        $liker->likes()->attach($post->id);
+        
+        $post->user->notify(new NotifyUponAction(
+            $liker,
+            config('api.notifications.post_liked')
+        ));
+
+        return [
+            'status' => 200,
+            'message' => 'Successfully liked a post.',
+        ];
+    }
+
+    /**
+     * Dislike a post.
+     * 
+     * @param \App\Models\User  $user
+     * @param string  $postId
+     * @return array
+     */
+    public function dislikePost(User $user, string $postId): array
+    {
+        $user->likes()->detach($postId);
+
+        return [
+            'status' => 200,
+            'message' => 'Successfully disliked a post.',
+        ];
+    }
+
+    /**
+     * Bookmark a post.
+     * 
+     * @param \App\Models\User  $user
+     * @param string  $postId
+     * @return array
+     */
+    public function bookmarkPost(User $user, string $postId): array
+    {
+        $user->bookmarks()->attach($postId);
+
+        return [
+            'status' => 200,
+            'message' => 'Successfully bookmarked a post.',
+        ];
+    }
+
+    /**
+     * Unbookmark a post.
+     * 
+     * @param \App\Models\User  $user
+     * @param string  $postId
+     * @return array
+     */
+    public function unbookmarkPost(User $user, string $postId): array
+    {
+        $user->bookmarks()->detach($postId);
+
+        return [
+            'status' => 200,
+            'message' => 'Successfully unbookmarked a post.',
+        ];
+    }
+}
