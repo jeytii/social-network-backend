aaeab775f6c554116781c7642beb12c2a0c6ec28
Jonathan Tarnate <jstarnate16@gmail.com>
2021-11-07 21:01:02 +0800

    Configured options.

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,19 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'login',
+        'register',
+        'verify/*',
+        'forgot-password',
+        'reset-password',
+        'sanctum/csrf-cookie'
+    ],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [config('app.client_url')],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +37,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
