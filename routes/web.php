9f8241144f90d6634554af654226ba25144fe727
Jonathan Tarnate <jstarnate16@gmail.com>
2021-12-05 02:05:37 +0800

    Made some small changes.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
+use App\Http\Controllers\{AuthController, ViewController};
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ViewController;
-use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
