eeb416c7b7eda70574de76d1f9427e485b21adb2
Jonathan Tarnate <jstarnate16@gmail.com>
2021-12-11 17:56:27 +0800

    Added a channel.

--- routes/channels.php
+++ routes/channels.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('notify.user.{slug}', function ($user, $slug) {
+    return $user->slug === $slug;
 });
