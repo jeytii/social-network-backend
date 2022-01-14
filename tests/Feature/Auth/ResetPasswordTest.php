fd96528e98997e4ed0bbc86ca24a0513073ef5b0
Jonathan Tarnate <jstarnate16@gmail.com>
2022-01-14 20:36:36 +0800

    Remove password_resets seeding and email attribute.

--- tests/Feature/Auth/ResetPasswordTest.php
+++ tests/Feature/Auth/ResetPasswordTest.php
@@ -1,17 +1,10 @@
 <?php
 
 use App\Models\User;
-use Illuminate\Support\Facades\{DB, Hash, Notification};
+use Illuminate\Support\Facades\{DB, Hash, Notification, Cache};
 
 beforeAll(function() {
-    $user = User::factory()->create();
-
-    $resets = collect(range(1, 7))->map(fn($reset) => [
-        'email' => $user->email,
-        'token' => bin2hex(random_bytes(16)),
-    ])->toArray();
-
-    DB::table('password_resets')->insert($resets);
+    User::factory()->create();
 });
 
 beforeEach(function() {
@@ -23,33 +16,26 @@ afterAll(function() {
     
     DB::table('users')->truncate();
     DB::table('password_resets')->truncate();
+    Cache::flush();
 });
 
 test('Should throw an error if token is missing', function() {
     $this->putJson(route('auth.reset-password'), [
-        'email' => $this->user->email,
-        'password' => 'P@ssword12345',
-        'password_confirmation' => 'P@ssword12345',
-    ])->assertStatus(422);
-});
-
-test('Should throw an error if email address is missing', function() {
-    $this->putJson(route('auth.reset-password'), [
-        'token' => bin2hex(random_bytes(16)),
-        'password' => 'P@ssword12345',
-        'password_confirmation' => 'P@ssword12345',
-    ])->assertStatus(422);
-});
-
-test('Should throw an error if token is invalid', function() {
-    $this->putJson(route('auth.reset-password'), [
-        'email' => $this->user->email,
-        'token' => '123456789',
         'password' => 'P@ssword12345',
         'password_confirmation' => 'P@ssword12345',
     ])
     ->assertStatus(422)
-    ->assertJsonValidationErrors(['email']);
+    ->assertJsonValidationErrors(['token']);
+});
+
+test('Should throw an error if token is invalid', function() {
+    $this->putJson(route('auth.reset-password'), [
+        'password' => 'P@ssword12345',
+        'password_confirmation' => 'P@ssword12345',
+        'token' => '123456789',
+    ])
+    ->assertStatus(422)
+    ->assertJsonValidationErrors(['token']);
 });
 
 test('Should successfully reset the password', function() {
@@ -57,17 +43,16 @@ test('Should successfully reset the password', function() {
     
     $this->postJson(route('auth.forgot-password'), $this->user->only('email'))
         ->assertOk();
-
-    $passwordReset = DB::table('password_resets')->where('email', $this->user->email)->first();
     
+    $passwordReset = DB::table('password_resets')->where('email', $this->user->email)->first();
+
     $this->putJson(route('auth.reset-password'), [
-        'email' => $passwordReset->email,
         'password' => 'P@ssword12345',
         'password_confirmation' => 'P@ssword12345',
         'token' => $passwordReset->token,
     ])->assertOk();
 
-    $user = DB::table('users')->where('email', $passwordReset->email)->first();
+    $user = DB::table('users')->where('email', $this->user->email)->first();
 
     $this->assertTrue(Hash::check('P@ssword12345', $user->password));
 });
